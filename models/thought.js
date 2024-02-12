5034614e2b6a5d5aa9a036e57a85f79d38e16882
TorresBones <jonathantorres521@gmail.com>
2024-02-12 17:29:22 -0500

    Completing the user controller and all the models

---
+++ models/thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+const dateFormat = require('../utils/dateFormat');
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Leave a thought.',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+const thought = model('thought', thoughtSchema);
+
+module.exports = thought;
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const thought = require("./thought");
+
+const { Schema, model } = require('mongoose');
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@+\..+/, 'Provide a valid email address.'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// userSchema.pre("findOneAndDelete", { document: false, query: true }, aysnc function() {
+//     console.log("User pre-delete");
+//     const doc = await this.model.findOne(this.getFilter());
+//     console.log("doc.username");
+//     await thought.deleteMany({ username: doc.username });
+// });
+
+const user = model('user'. userSchema);
+module.exports = user;
