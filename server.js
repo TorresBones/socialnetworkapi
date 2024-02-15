72adbedf8be7245d4fc7cc8f15b36ef3b1959e83
TorresBones <jonathantorres521@gmail.com>
2024-02-15 18:42:19 -0500

    Debugging server.js

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('publuc'));
+app.use(express.static('public'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkapi', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkapi');
 
-mongoose.set("debug", true);
+// mongoose.set("debug", true);
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(``))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
