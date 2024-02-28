4d803a49eb28fb119bd4ff182a3ce4c6dcee929c
TorresBones <jonathantorres521@gmail.com>
2024-02-27 21:31:31 -0500

    Changing dateFormat.js

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -36,7 +36,7 @@ module.exports = (timestamp, { monthLength = 'short', dateSuffix = true } = {}) 
     const dayOfMonth = dateSuffix ? addDateSuffix(dateObj.getDate()) : dateObj.getDate();
     const year = dateObj.getFullYear();
     let hour = dateObj.getHours() > 12 ? dateObj.getHours() - 12 : dateObj.getHours();
-    hour = hour === 0 ? 12 : hour; // Ensure hour is not 0
+    hour = hour === 0 ? 12 : hour;
     const minutes = dateObj.getMinutes();
     const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
 
