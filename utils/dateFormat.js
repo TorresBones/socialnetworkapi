80e4aa1ae09704cb44d3da033a00044441f9f704
TorresBones <jonathantorres521@gmail.com>
2024-02-27 19:40:39 -0500

    Debugging reaction.js and dateFormat.js

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,96 +1,46 @@
 const addDateSuffix = date => {
     let dateString = date.toString();
-
     const lastChar = dateString.charAt(dateString.length - 1);
 
     if (lastChar === '1' && dateString !== '11') {
-        dateString = `${dateString}st`;
+        dateString += 'st';
     } else if (lastChar === '2' && dateString !== '12') {
-        dateString = `${dateString}nd`;
+        dateString += 'nd';
     } else if (lastChar === '3' && dateString !== '13') {
-        dateString = `${dateString}rd`;
+        dateString += 'rd';
     } else {
-        dateString = `${dateString}th`;
+        dateString += 'th';
     }
 
     return dateString;
 };
 
-module.exports = (
-    timestamp,
-    { monthLength = 'short', dateSuffix = true } = {}
-) => {
-    let months;
-
-    if (monthLength === 'short') {
-        months = {
-            0: 'Jan',
-            1: 'Feb',
-            2: 'Mar',
-            3: 'Apr',
-            4: 'May',
-            5: 'Jun',
-            6: 'Jul',
-            7: 'Aug',
-            8: 'Sep',
-            9: 'Oct',
-            10: 'Nov',
-            11: 'Dec'
-        };
-    } else {
-        months = {
-            0: 'January',
-            1: 'February',
-            2: 'March',
-            3: 'April',
-            4: 'May',
-            5: 'June',
-            6: 'July',
-            7: 'August',
-            8: 'September',
-            9: 'October',
-            10: 'November',
-            11: 'December'
-        };
-    }
+module.exports = (timestamp, { monthLength = 'short', dateSuffix = true } = {}) => {
+    const months = {
+        0: monthLength === 'short' ? 'Jan' : 'January',
+        1: monthLength === 'short' ? 'Feb' : 'February',
+        2: monthLength === 'short' ? 'Mar' : 'March',
+        3: monthLength === 'short' ? 'Apr' : 'April',
+        4: 'May',
+        5: 'Jun',
+        6: 'Jul',
+        7: 'Aug',
+        8: 'Sep',
+        9: 'Oct',
+        10: 'Nov',
+        11: 'Dec'
+    };
 
     const dateObj = new Date(timestamp);
     const formatMonth = months[dateObj.getMonth()];
+    const dayOfMonth = dateSuffix ? addDateSuffix(dateObj.getDate()) : dateObj.getDate();
+    const year = dateObj.getFullYear();
+    let hour = dateObj.getHours() > 12 ? dateObj.getHours() - 12 : dateObj.getHours();
+    hour = hour === 0 ? 12 : hour; // Ensure hour is not 0
+    const minutes = dateObj.getMinutes();
+    const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
 
-    let dayOfMonth;
-     
-    if (dateSuffix) {
-        dayOfMonth = addDateSuffix(dateObj.getDate());
-} else {
-    dayOfMonth = dateObj.getDate();
-}
+    const formatTimeStamp = `${formatMonth} ${dayOfMonth} ${year} at ${hour}:${minutes < 10 ? '0' : ''}${minutes} ${periodOfDay}`;
 
-const year = dateObj.getFullYear();
-
-let hour;
-
-if (dateObj.getHours > 12) {
-    hour = Math.floor(dateObj.getHours() / 2);
-} else {
-    hour = dateObj.getHours();
-}
-
-if (hour === 0) {
-    hour - 12;
-}
-
-const minutes = dateObj.getMinutes();
-
-let periodOfDay;
-
-if (dateObj.getHours() >= 12) {
-    periodOfDay = 'pm';
-} else {
-    periodOfDay = 'am';
-}
-
-const formatTimeStamp = `${formatMonth} ${dayOfMonth} ${year} at ${hour}:${minutes} ${periodOfDay}`;
-
-return formatTimeStamp;
-
+    return formatTimeStamp;
 };
